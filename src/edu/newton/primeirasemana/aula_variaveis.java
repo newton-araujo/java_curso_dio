786fd61b86dd1b2c88de218efb3ee5985e9f0f7b
Newton Araújo | Developer <127565702+newton-araujo@users.noreply.github.com>
2024-07-18 22:33:25 -0300

    aula_variaveis

---
+++ src/edu/newton/primeirasemana/aula_variaveis.java
@@ -0,0 +1,20 @@
+package edu.newton.primeirasemana;
+
+public class aula_variaveis {
+
+    public static void main(String[] args) {
+        double salario = 2500.00;
+        int anoDeNascimento = 1993;
+        String nome = "Newton";
+        boolean estudando = true;
+
+        System.out.println(nome);
+        System.out.println(salario);
+        System.out.println(estudando);
+        System.out.println(anoDeNascimento);
+
+        }
+        
+
+
+}
---
+++ src/edu/newton/primeirasemana/aula_variaveis2.java
@@ -0,0 +1,13 @@
+package edu.newton.primeirasemana;
+
+public class aula_variaveis2 {
+
+    public static void main(String[] args) {
+        
+        final double PI = 3.14; //Usando o final antes do tipo, não poderá ser alterado;
+
+        System.out.println(PI);
+
+    }
+    
+}
