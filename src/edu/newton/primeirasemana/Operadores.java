ae8f2092734e470a554d532feec136af8f253668
Newton Araújo | Developer <127565702+newton-araujo@users.noreply.github.com>
2024-07-20 13:55:45 -0300

    aula_operadores

---
+++ src/edu/newton/primeirasemana/Operadores.java
@@ -0,0 +1,36 @@
+package edu.newton.primeirasemana;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        
+        double soma = 15.7 + 12.4;
+        int subtrair = 5 - 2;
+        int multiplicacao = 2 * 2;
+        int divisao = 10 / 2;
+        int restoDaDivisao = 10 % 2;
+        double modulo = (2 * 2) + (5 / 2);
+
+
+        //Concatenando String
+
+        String nomeCompleto = "Linguagem" + " Java";
+
+
+        String concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1+1+1);
+
+        System.out.println(concatenacao);
+
+    }
+
+    
+    
+}
