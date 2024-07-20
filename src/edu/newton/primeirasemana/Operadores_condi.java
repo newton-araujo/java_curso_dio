9ed202032e4724b6c9755e8a9446be9b1dda99a9
Newton Araújo | Developer <127565702+newton-araujo@users.noreply.github.com>
2024-07-20 20:40:33 -0300

    condicionais

---
+++ src/edu/newton/primeirasemana/Operadores_condi.java
@@ -0,0 +1,20 @@
+package edu.newton.primeirasemana;
+
+public class Operadores_condi {
+    
+    public static void main(String[] args) {
+        
+        int a, b;
+    
+        a = 2;
+        b = 5;
+    
+        if (a == b){
+            System.out.println("a é igual a b");
+        } else if (a < b) {
+            System.out.println("a é menor que b");
+        } else if(a > b) {
+            System.out.println("a é maior que b");
+        } 
+    }
+}
