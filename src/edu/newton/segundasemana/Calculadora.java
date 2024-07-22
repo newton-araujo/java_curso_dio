0bfea16c074a093731e9a1d073f0ce4ba0acd304
Newton Araújo | Developer <127565702+newton-araujo@users.noreply.github.com>
2024-07-21 22:15:46 -0300

    doc

---
+++ src/edu/newton/segundasemana/Calculadora.java
@@ -0,0 +1,28 @@
+package edu.newton.segundasemana;
+/**
+ * <h1>Calculadora</h1>
+ * 
+ * A Calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes para
+ * desfrutar dos recursos oferecidos pelo o autor
+ * 
+ * 
+ * @author Newton Araujo
+ * @version 1.0
+ * @since 21/07/2024
+ */
+public class Calculadora {
+    /**
+     * Este metodo é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     * 
+     */
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+} 
+
+
